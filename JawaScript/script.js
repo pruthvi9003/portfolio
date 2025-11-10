5eb9066143077f153a7c3141f0e30d781b020bf5
Pruthvi Potdar <pruthvipotdar2006@gmail.com>
2025-11-10 20:37:00 +0530

    Add files via upload

---
+++ JawaScript/script.js
@@ -0,0 +1,10 @@
+// Add smooth scroll animation for nav links
+const links = document.querySelectorAll("nav a");
+
+links.forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    const target = document.querySelector(link.getAttribute("href"));
+    target.scrollIntoView({ behavior: "smooth" });
+  });
+});
